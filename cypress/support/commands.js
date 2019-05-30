08e4947f9ddfb27b13ca7cf368d567c194a71693
andreidanchis <andrei.chis@evision-software.com>
2019-05-30 21:40:07 +0200

    added commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,5 +1,14 @@
-Cypress.Commands.add('seedAndVisit', (seedData = 'fixture:todos') => {
-  cy.server()
-  cy.route('GET', '/api/todos', seedData)
-  cy.visit('/')
+Cypress.Commands.add('login', () => {
+  cy.visit('/login')
+  cy.get('#email').type('test@test.com')
+  cy.get('#password').type('testaccount')
+  cy.get('.col-md-12 > .btn-primary')
+    .contains('Login').click()
+})
+
+Cypress.Commands.add('getReport', () => {
+  cy.visit('/report/96a0fa1f-6a93-4947-8a69-9314f0592788')
+  cy.get('#section_property > :nth-child(1)')
+            .contains('The property')
+            .should('be.visible')
 })
